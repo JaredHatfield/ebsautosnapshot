f6c7a1fb9f339a70ca723a3cb7f54a0fdef85f52
Jared Hatfield <jjhatf02@gmail.com>
2014-12-06 13:45:29 -0500

    New Maven project.

---
+++ src/main/java/com/unitvectory/ebsautosnapshot/App.java
@@ -0,0 +1,53 @@
+package com.unitvectory.ebsautosnapshot;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Logger;
+
+/**
+ * The main application.
+ *
+ * @author Jared Hatfield
+ *
+ */
+public class App {
+
+	/**
+	 * the log
+	 */
+	private static Logger log = Logger.getLogger(App.class);
+
+	/**
+	 * The main application.
+	 *
+	 * @param args
+	 *            the args
+	 */
+	public static void main(String[] args) {
+		// create the parser
+		org.apache.commons.cli.CommandLineParser parser = new BasicParser();
+		try {
+			// The required options
+			Options options = new Options();
+			@SuppressWarnings("static-access")
+			Option configOption = OptionBuilder.withArgName("config").hasArg()
+					.withDescription("path to config file").isRequired()
+					.create("config");
+			options.addOption(configOption);
+
+			// Parse the command line arguments
+			CommandLine line = parser.parse(options, args);
+			String configPath = line.getOptionValue("config");
+
+			// TODO: do stuff here
+		} catch (ParseException exp) {
+			System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+		} catch (Exception e) {
+			log.error("Failed", e);
+		}
+	}
+}
